da87ac8379901a51110d108c5253b843f50cd0fb
Dunkaburk <jonte8000@hotmail.com>
2022-09-08 14:27:57 +0200

    Added base attributes to character class and interfaces that Character class requires

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,4 +1,13 @@
+import java.util.List;
+
 public class Character {
+    private String name;
+    private Race race;
+    private Job job; // Job represents a D&D Class since "class" is otherwise a keyword in java.
+    private List<Feature> feats;
+    private int health;
+    private int armorClass;
+
 
     public Character(){
 
---
+++ src/main/java/Race.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public interface Race {
+    public String getRaceName();
+
+    public int getMovementSpeed();
+
+    public List<String> getRaceActions();
+
+}
