34fe6d450033e23b0426206d7a4c8b95ecb7b294
Dunkaburk <jonte8000@hotmail.com>
2022-09-08 15:05:51 +0200

    Worked on Character class and its attributes, added data type for inventory item

--- src/main/java/FXBaseTestScreen.java
+++ src/main/java/FXBaseTestScreen.java
@@ -1,2 +1,25 @@
-public class FXBaseTestScreen {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class FXBaseTestScreen extends Application {
+
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+
+        Label l = new Label("Här står saker:, JavaFX version:" + javafxVersion + ", Java på version" + javaVersion + ".");
+
+
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
