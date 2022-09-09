596901dbdfc4ce4518045004bea56b44315498a5
02list15 <02list15@gmail.com>
2022-09-09 13:15:16 +0200

    Made private method public for use

--- src/main/java/DiceBag.java
+++ src/main/java/DiceBag.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class DiceBag {
 
-    private static int rollDice(int diceType){
+    public static int rollDice(int diceType){
         Random rand = new Random();
         return rand.nextInt(diceType)+1; // Generates a random number between 1 and the max of the dice type.
     }
