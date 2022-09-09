ee49c255e5f7de180c8ca0c59ddd665db84aec56
Vikmag9 <93663405+Vikmag9@users.noreply.github.com>
2022-09-09 12:48:10 +0200

    Created Character_test class

---
+++ src/main/java/Stats.java
@@ -0,0 +1,26 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stats {
+    int inte;
+    int cha;
+    int wis;
+    int con;
+    int dex;
+    int str;
+    List<Integer> listOfRolls;
+
+    public Stats(){
+
+    }
+
+    private void rollStat(){
+        listOfRolls = new ArrayList<Integer>();
+        for(int i = 1; i < 5; i++){
+
+        }
+    }
+}
+
+
