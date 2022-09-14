41549e6e6ae288e49d0e0fdd48f3bd229bb31b6c
Dunkaburk <jonte8000@hotmail.com>
2022-09-14 15:45:29 +0200

    Maven changes, maven now builds, project compiles and javafx runs

--- src/main/java/Barbarian.java
+++ src/main/java/Barbarian.java
@@ -15,8 +15,9 @@ public class Barbarian implements Job{
     FileManager fm = new FileManager();
     public HashMap getJob() throws IOException {
         JSONObject jsonJob = fm.readFile(new File("")); // TODO Insert path to job JSON file
-        if (jsonJob.toMap().containsKey("Barbarian"))
-        {
+        Map jobMap = jsonJob.toMap();
+        if (jobMap.containsKey("Barbarian")) {
+            jobMap.get("Barbarian"); // Cannot be tested atm since application does not run. Need to run to figure out how to get barb information
             // TODO map json file into a Map only containing barbarian relevant information
         }
         return null;
