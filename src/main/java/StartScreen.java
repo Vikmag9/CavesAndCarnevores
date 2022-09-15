297e22d0d8dbc6927261c7945848bd7713191655
02list15 <02list15@gmail.com>
2022-09-15 11:01:28 +0200

    Connected FXML IDs to StartScreen.java class and created placeholder methods for the buttons.

--- src/main/java/StartScreen.java
+++ src/main/java/StartScreen.java
@@ -1,3 +1,23 @@
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class StartScreen {
-    
+    @FXML
+    private Button createCharacterHeaderButton;
+    @FXML
+    private Button myCharactersHeaderButton;
+    @FXML
+    private Button createCharacterButton;
+    @FXML
+    private Button myCharactersButton;
+
+    public static void createCharacter(){
+        // Sends user to the Character Creator Wizard
+    }
+
+    public static void myCharacters(){
+        // Sends user to the Characters Screen with previously created characters
+    }
+
 }
