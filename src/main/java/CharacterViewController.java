ba46e5e6e5e9ca0e242c1e686663f75d197a1102
02list15 <02list15@gmail.com>
2022-10-13 15:57:17 +0200

    Added ItemsListItem.fxml and designed it. Also added fx:ids to Basics, Combat, and Inventory in CharacterInfoScreen.fxml to CharacterViewController.

--- src/main/java/CharacterViewController.java
+++ src/main/java/CharacterViewController.java
@@ -5,11 +5,76 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.*;
 
 public class CharacterViewController implements Initializable {
+
+    //Basics tab fx:ids
+    @FXML Text strengthsPtsText;
+    @FXML Text dexterityPtsText;
+    @FXML Text constitutionPtsText;
+    @FXML Text wisdomPtsText;
+    @FXML Text intelligencePtsText;
+    @FXML Text charismaPtsText;
+
+    @FXML Text strengthModifierText;
+    @FXML Text dexterityModifierText;
+    @FXML Text constitutionModifierText;
+    @FXML Text wisdomModifierText;
+    @FXML Text intelligenceModifierText;
+    @FXML Text charismaModifierText;
+
+    @FXML Circle strengthSavingThrowCircle;
+    @FXML Circle dexteritySavingThrowCircle;
+    @FXML Circle constitutionSavingThrowCircle;
+    @FXML Circle wisdomSavingThrowCircle;
+    @FXML Circle intelligenceSavingThrowCircle;
+    @FXML Circle charismaSavingThrowCircle;
+
+    @FXML Circle athleticsSkillStatus;
+    @FXML Circle acrobaticsSkillStatus;
+    @FXML Circle sleightOfHandSkillStatus;
+    @FXML Circle stealthSkillStatus;
+    @FXML Circle arcanaSkillStatus;
+    @FXML Circle historySkillStatus;
+    @FXML Circle investigationSkillStatus;
+    @FXML Circle natureSkillStatus;
+    @FXML Circle religionSkillStatus;
+    @FXML Circle animalHandlingSkillStatus;
+    @FXML Circle insightSkillStatus;
+    @FXML Circle medicineSkillStatus;
+    @FXML Circle perceptionSkillStatus;
+    @FXML Circle survivalSkillStatus;
+    @FXML Circle deceptionSkillStatus;
+    @FXML Circle intimidationSkillStatus;
+    @FXML Circle performanceSkillStatus;
+    @FXML Circle persuasionSkillStatus;
+
+    @FXML TextArea spellsTextArea;
+    @FXML TextArea languagesTextArea;
+    @FXML TextArea proficienciesTextArea;
+
+
+    // Combat fx:ids
+
+    @FXML private Text strengthCombatPtsText;
+    @FXML private Text dexterityCombatPtsText;
+    @FXML private Text constitutionCombatPtsText;
+    @FXML private Text wisdomCombatPtsText;
+    @FXML private Text intelligenceCombatPtsText;
+    @FXML private Text charismaCombatPtsText;
+
+    @FXML private Text strengthCombatModifierPtsText;
+    @FXML private Text dexterityCombatModifierPtsText;
+    @FXML private Text constitutionCombatModifierPtsText;
+    @FXML private Text wisdomCombatModifierPtsText;
+    @FXML private Text intelligenceCombatModifierPtsText;
+    @FXML private Text charismaCombatModifierPtsText;
+
     @FXML private TextArea actionsTextArea;
     @FXML private TextArea reactionsTextArea;
     @FXML private TextArea bonusActionsTextArea;
@@ -26,6 +91,24 @@ public class CharacterViewController implements Initializable {
     @FXML private CheckBox acCheckBox;
     @FXML private CheckBox atkBonusCheckBox;
     @FXML private CheckBox isMagicalCheckBox;
+
+    // Spells fx:ids
+
+    // Lore & Notes fx:ids
+
+    // Inventory fx:ids
+    @FXML private Text itemNameText;
+    @FXML private Text itemTypeText;
+    @FXML private Text itemMagicalText;
+    @FXML private Text itemAttackBonusText;
+    @FXML private Text itemACText;
+    @FXML private Text itemValueText;
+
+    @FXML private TextArea itemDescriptionTextArea;
+
+    @FXML private Button itemSellButton;
+    @FXML private Button itemRemoveButton;
+    @FXML private Button itemConsumeButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -56,7 +139,7 @@ public class CharacterViewController implements Initializable {
 
     private void setReactionsTextArea() {
         String reactions = """
-                - Opporty Attack
+                - Opportunity Attack
                 - Perform a readied action
                 - Cast a spell
                 """;
