33fb65a62c18987c4fe9388f78a21f64d0baab23
02list15 <02list15@gmail.com>
2022-10-14 10:12:13 +0200

    Added all needed fx:ids and fixed warnings in CharacterInfoScreen.fxml, CharacterViewController, ItemsListItem.fxml.

--- src/main/java/CharacterViewController.java
+++ src/main/java/CharacterViewController.java
@@ -13,50 +13,70 @@ import java.util.*;
 
 public class CharacterViewController implements Initializable {
 
+
     //Basics tab fx:ids
-    @FXML Text strengthsPtsText;
-    @FXML Text dexterityPtsText;
-    @FXML Text constitutionPtsText;
-    @FXML Text wisdomPtsText;
-    @FXML Text intelligencePtsText;
-    @FXML Text charismaPtsText;
+    @FXML private Text strengthsPtsText;
+    @FXML private Text dexterityPtsText;
+    @FXML private Text constitutionPtsText;
+    @FXML private Text wisdomPtsText;
+    @FXML private Text intelligencePtsText;
+    @FXML private Text charismaPtsText;
 
-    @FXML Text strengthModifierText;
-    @FXML Text dexterityModifierText;
-    @FXML Text constitutionModifierText;
-    @FXML Text wisdomModifierText;
-    @FXML Text intelligenceModifierText;
-    @FXML Text charismaModifierText;
+    @FXML private Text strengthModifierText;
+    @FXML private Text dexterityModifierText;
+    @FXML private Text constitutionModifierText;
+    @FXML private Text wisdomModifierText;
+    @FXML private Text intelligenceModifierText;
+    @FXML private Text charismaModifierText;
 
-    @FXML Circle strengthSavingThrowCircle;
-    @FXML Circle dexteritySavingThrowCircle;
-    @FXML Circle constitutionSavingThrowCircle;
-    @FXML Circle wisdomSavingThrowCircle;
-    @FXML Circle intelligenceSavingThrowCircle;
-    @FXML Circle charismaSavingThrowCircle;
+    @FXML private Circle strengthSavingThrowCircle;
+    @FXML private Circle dexteritySavingThrowCircle;
+    @FXML private Circle constitutionSavingThrowCircle;
+    @FXML private Circle wisdomSavingThrowCircle;
+    @FXML private Circle intelligenceSavingThrowCircle;
+    @FXML private Circle charismaSavingThrowCircle;
 
-    @FXML Circle athleticsSkillStatus;
-    @FXML Circle acrobaticsSkillStatus;
-    @FXML Circle sleightOfHandSkillStatus;
-    @FXML Circle stealthSkillStatus;
-    @FXML Circle arcanaSkillStatus;
-    @FXML Circle historySkillStatus;
-    @FXML Circle investigationSkillStatus;
-    @FXML Circle natureSkillStatus;
-    @FXML Circle religionSkillStatus;
-    @FXML Circle animalHandlingSkillStatus;
-    @FXML Circle insightSkillStatus;
-    @FXML Circle medicineSkillStatus;
-    @FXML Circle perceptionSkillStatus;
-    @FXML Circle survivalSkillStatus;
-    @FXML Circle deceptionSkillStatus;
-    @FXML Circle intimidationSkillStatus;
-    @FXML Circle performanceSkillStatus;
-    @FXML Circle persuasionSkillStatus;
+    @FXML private Circle athleticsSkillStatus;
+    @FXML private Circle acrobaticsSkillStatus;
+    @FXML private Circle sleightOfHandSkillStatus;
+    @FXML private Circle stealthSkillStatus;
+    @FXML private Circle arcanaSkillStatus;
+    @FXML private Circle historySkillStatus;
+    @FXML private Circle investigationSkillStatus;
+    @FXML private Circle natureSkillStatus;
+    @FXML private Circle religionSkillStatus;
+    @FXML private Circle animalHandlingSkillStatus;
+    @FXML private Circle insightSkillStatus;
+    @FXML private Circle medicineSkillStatus;
+    @FXML private Circle perceptionSkillStatus;
+    @FXML private Circle survivalSkillStatus;
+    @FXML private Circle deceptionSkillStatus;
+    @FXML private Circle intimidationSkillStatus;
+    @FXML private Circle performanceSkillStatus;
+    @FXML private Circle persuasionSkillStatus;
 
-    @FXML TextArea spellsTextArea;
-    @FXML TextArea languagesTextArea;
-    @FXML TextArea proficienciesTextArea;
+    @FXML private Text athleticsSkillLevelText;
+    @FXML private Text acrobaticsSkillLevelText;
+    @FXML private Text sleightOfHandSkillLevelText;
+    @FXML private Text stealthSkillLevelText;
+    @FXML private Text arcanaSkillLevelText;
+    @FXML private Text historySkillLevelText;
+    @FXML private Text investigationSkillLevelText;
+    @FXML private Text natureSkillLevelText;
+    @FXML private Text religionSkillLevelText;
+    @FXML private Text animalHandlingSkillLevelText;
+    @FXML private Text insightSkillLevelText;
+    @FXML private Text medicineSkillLevelText;
+    @FXML private Text perceptionSkillLevelText;
+    @FXML private Text survivalSkillLevelText;
+    @FXML private Text deceptionSkillLevelText;
+    @FXML private Text intimidationSkillLevelText;
+    @FXML private Text performanceSkillLevelText;
+    @FXML private Text persuasionSkillLevelText;
+
+    @FXML private TextArea spellsTextArea;
+    @FXML private TextArea languagesTextArea;
+    @FXML private TextArea proficienciesTextArea;
 
 
     // Combat fx:ids
@@ -68,12 +88,16 @@ public class CharacterViewController implements Initializable {
     @FXML private Text intelligenceCombatPtsText;
     @FXML private Text charismaCombatPtsText;
 
-    @FXML private Text strengthCombatModifierPtsText;
-    @FXML private Text dexterityCombatModifierPtsText;
-    @FXML private Text constitutionCombatModifierPtsText;
-    @FXML private Text wisdomCombatModifierPtsText;
-    @FXML private Text intelligenceCombatModifierPtsText;
-    @FXML private Text charismaCombatModifierPtsText;
+    @FXML private Text strengthCombatModifierText;
+    @FXML private Text dexterityCombatModifierText;
+    @FXML private Text constitutionCombatModifierText;
+    @FXML private Text wisdomCombatModifierText;
+    @FXML private Text intelligenceCombatModifierText;
+    @FXML private Text charismaCombatModifierText;
+
+    @FXML private Text acText;
+    @FXML private Text initiativeText;
+    @FXML private Text movementSpeedText;
 
     @FXML private TextArea actionsTextArea;
     @FXML private TextArea reactionsTextArea;
@@ -84,6 +108,32 @@ public class CharacterViewController implements Initializable {
     List<List<String>> spellList;
     Character character;
 
+
+
+    // Spells fx:ids
+
+    @FXML private Button level1SpellsButton;
+    @FXML private Button level2SpellsButton;
+    @FXML private Button level3SpellsButton;
+    @FXML private Button level4SpellsButton;
+    @FXML private Button level5SpellsButton;
+    @FXML private Button level6SpellsButton;
+    @FXML private Button level7SpellsButton;
+    @FXML private Button level8SpellsButton;
+    @FXML private Button level9SpellsButton;
+
+
+    // Lore & Notes fx:ids
+
+    @FXML private TextArea earlierLifeTextArea;
+    @FXML private TextArea organizationTextArea;
+    @FXML private TextArea coreMemoriesTextArea;
+    @FXML private TextArea notesTextArea;
+
+    @FXML private Button loreAndNotesSaveButton;
+
+    // Inventory fx:ids
+
     @FXML private RadioButton equippableRadioButton;
     @FXML private RadioButton consumableRadioButton;
     @FXML private RadioButton miscellaneousRadioButton;
@@ -92,11 +142,17 @@ public class CharacterViewController implements Initializable {
     @FXML private CheckBox atkBonusCheckBox;
     @FXML private CheckBox isMagicalCheckBox;
 
-    // Spells fx:ids
+    @FXML private FlowPane inventoryFlowPane;
+    @FXML private TextField currentGoldTextField;
 
-    // Lore & Notes fx:ids
+    @FXML private TextField itemNameTextField;
+    @FXML private TextField itemValueTextField;
+    @FXML private TextArea itemCreationDescriptionTextArea;
 
-    // Inventory fx:ids
+
+    @FXML private Button createItemButton;
+    @FXML private Button clearItemCreationButton;
+
     @FXML private Text itemNameText;
     @FXML private Text itemTypeText;
     @FXML private Text itemMagicalText;
@@ -117,6 +173,10 @@ public class CharacterViewController implements Initializable {
         setBonusActionsTextArea();
         prepareRadioButtons();
     }
+
+    // Basics features
+
+    // Combat features
 
     private void setActionsTextArea() {
         String actions = """
@@ -229,5 +289,12 @@ public class CharacterViewController implements Initializable {
             }
         });
     }
+
+    // Spells features
+
+    // Lore & Notes features
+
+    // Inventory features
+
 
 }
