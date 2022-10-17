880cbcb893d0db986d162ced20e14a374c8aa503
Bat <sarahedb@net.chalmers.se>
2022-10-17 19:29:27 +0200

    solved so that the start screen loads again but the others dont work after the refractoring

--- src/main/java/Controller/SceneController.java
+++ src/main/java/Controller/SceneController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -14,6 +15,7 @@ import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -27,8 +29,9 @@ public class SceneController implements Initializable {
     //navigational methods to switch fxml files in the center part of the border pane
         @FXML protected void openStartScreen() {
         try {
-
-            AnchorPane startScreen = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
+            FXMLLoader loader = new FXMLLoader();
+            URL fxmlURL = Paths.get("src/main/resources/StartScreen.fxml").toUri().toURL();
+            Parent startScreen = loader.load(fxmlURL);
             borderPane.setCenter(startScreen);
         } catch (Exception e) {
             System.out.println("Error loading FXML file: " + e);
@@ -37,7 +40,11 @@ public class SceneController implements Initializable {
 
     @FXML public void openCreateCharacterScreen() throws IOException, ClassNotFoundException {
         try {
-            AnchorPane startScreen = FXMLLoader.load(getClass().getResource("Wizard.fxml"));
+            FXMLLoader loader = new FXMLLoader();
+            URL fxmlURL = Paths.get("src/main/resources/Wizard.fxml").toUri().toURL();
+            Parent startScreen = loader.load(fxmlURL);
+
+
             borderPane.setCenter(startScreen);
         } catch (Exception e) {
             System.out.println("Error loading FXML file: " + e);
