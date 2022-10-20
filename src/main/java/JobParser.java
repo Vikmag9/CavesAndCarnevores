e816768870d4498cd1c12f7f03183b7af0445d80
Bat <sarahedb@net.chalmers.se>
2022-10-20 14:20:43 +0200

    added javadoc comments and fixed the last of the parsing in Jobparser. Also removed some unused methods in the jobparser

--- src/main/java/JobParser.java
+++ src/main/java/JobParser.java
@@ -6,7 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-
+/**
+ * This class parses the Json file job.json and is used by the job class.
+ * It contains the methods to retrieve the information from the Json file.
+ * This is to avoid the job class from being directly dependent on the Json file.
+ */
 public class JobParser {
     private String jobName;
     private int hitdie;
@@ -15,6 +19,12 @@ public class JobParser {
     private JSONObject jobContent;
     static private final FileManager fm = new FileManager();
 
+    /**
+     * The constructor for the JobParser class.
+     * @param jobName The name of the job, as a string.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     public JobParser(String jobName) throws IOException, ClassNotFoundException {
         this.jobName = jobName;
         this.jobContent = getJob(jobName);
@@ -25,10 +35,22 @@ public class JobParser {
 
 
 
+    /**
+     * A method to retrieve the information from the Json file.
+     * @param jobName The name of the job, as a string.
+     * @return The information from the Json file, as a JSONObject.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     public JSONObject getJob(String jobName) throws IOException, ClassNotFoundException {
         JSONObject jsonJob = fm.readFile("jobs.json").getJSONObject(jobName);
         return jsonJob;
     }
+
+    /**
+     * a method to retrieve the features of a class from the Json file.
+     * @return A list of features, as a list of Feature objects.
+     */
     public List<Feature> parseFeatures() {
         ArrayList<Feature> features = new ArrayList<>();
         JSONObject featureTable = jobContent.getJSONObject("Class Features").getJSONObject("The "+this.jobName).getJSONObject("table");
@@ -44,7 +66,13 @@ public class JobParser {
         return features;
     }
 
-    public static List<String> getAllJobs() throws IOException, ClassNotFoundException { // TODO: To be used when displaying all available jobs in the UI
+    /**
+     * A method to retrieve all the job names from the Json file.
+     * @return A list of job names, as a list of strings.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static List<String> getAllJobs() throws IOException, ClassNotFoundException {
         JSONObject jobs = fm.readFile("jobs.json");
         List<String> jobNames = new ArrayList<>();
         jobs.keySet().forEach(jobName -> {
@@ -64,6 +92,10 @@ public class JobParser {
         return hitDie;
     }
 
+    /**
+     * A method to retrieve and parse the proficiencies of a class from the Json file.
+     * @return A map of proficiencies, as a map of strings and lists of strings.
+     */
     public Map parseProficiencies() {
         jobContent.getJSONObject("Class Features").getJSONObject("Proficiencies");
         return jobContent.getJSONObject("Class Features").getJSONObject("Proficiencies").toMap();
@@ -79,18 +111,6 @@ public class JobParser {
 
     public List getEquipment() {
         return jobContent.getJSONObject("Class Features").getJSONObject("Equipment").getJSONArray("content").toList();
-    }
-
-    public void setProficiencies(Map proficiencies) {
-        this.proficiencies = proficiencies;
-    }
-
-    public void setFeatures(List<Feature> features) {
-        this.features = features;
-    }
-
-    public void setJobContent(JSONObject jobContent) {
-        this.jobContent = jobContent;
     }
 
     public int getHitdie() {
